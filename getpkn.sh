9d97f7ad9ae56686c5a411d8cea1ebb41dba5d5f
Dr. John <jjtseng93@gmail.com>
2025-11-13 23:19:27 +0800

    Fix getpkn run in Termux

--- getpkn.sh
+++ getpkn.sh
@@ -9,18 +9,22 @@ scriptdir=$(dirname $(realpath "$0"))
 # These 2 for both Termux and Guest app
 if ! [ -z $PKG_NAME ] ; then
   package="$PKG_NAME"
-elif echo $scriptdir | grep 'Android/media' ; then
-  package=$(echo $scriptdir | grep -o -E 'Android/media/[^/]+' | grep -o -E '[^/]+' | sed -n '3p')
 
-elif echo $scriptdir | grep -E '^/data/data/' ; then
-  package=$(echo $scriptdir | grep -o -E 'data/data/[^/]+' | grep -o -E '[^/]+' | sed -n '3p')
+elif [ -z "$PREFIX" ] ; then
+  if echo $scriptdir | grep 'Android/media' ; then
+    package=$(echo $scriptdir | grep -o -E 'Android/media/[^/]+' | grep -o -E '[^/]+' | sed -n '3p')
 
-  PKG_PDIR=$(echo $scriptdir | grep -o -E '^/data/data/[^/]+')
+  elif echo $scriptdir | grep -E '^/data/data/' ; then
+    package=$(echo $scriptdir | grep -o -E 'data/data/[^/]+' | grep -o -E '[^/]+' | sed -n '3p')
+
+    PKG_PDIR=$(echo $scriptdir | grep -o -E '^/data/data/[^/]+')
   
-elif echo $scriptdir | grep -E '^/data/user/' ; then
-  package=$(echo $scriptdir | grep -o -E 'data/user/[^/]+/[^/]+' | grep -o -E '[^/]+' | sed -n '4p')
+  elif echo $scriptdir | grep -E '^/data/user/' ; then
+    package=$(echo $scriptdir | grep -o -E 'data/user/[^/]+/[^/]+' | grep -o -E '[^/]+' | sed -n '4p')
 
-  PKG_PDIR=$(echo $scriptdir | grep -o -E '^/data/user/[^/]+/[^/]+')
+    PKG_PDIR=$(echo $scriptdir | grep -o -E '^/data/user/[^/]+/[^/]+')
+
+  fi
 
 elif [ -f $scriptdir/pkg_name.txt  ] ; then
   package=$(cat $scriptdir/pkg_name.txt)
